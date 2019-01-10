92a2b6966e9ca5aaffb511c973ec7fa523de08c9
willneedit <willneedit@nurfuerspam.de>
2019-01-10 16:54:31 +0100

    Added a Doorguard that allows me to ban misbehaving clients

---
+++ src/DoorGuard.ts
@@ -0,0 +1,38 @@
+/*!
+ * Copyright (c) iwontsay/willneedit. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import pgescape from 'pg-escape';
+import PGBackend from "./pg_backend";
+
+import { QueryResult } from 'pg';
+
+interface BanState {
+    lastCheck: number;
+    state: boolean;
+}
+
+export default class DoorGuard {
+    private static db = PGBackend.instance;
+
+    private static banList: { [id: string]: BanState } = { };
+
+    public static async init() {
+        await this.db.query('CREATE TABLE IF NOT EXISTS banned (' +
+            'ip varchar PRIMARY KEY NOT NULL)');
+    }
+
+    public static async isAdmitted(ip: string) {
+        const entry = this.banList[ip];
+        const currentTime = new Date().getTime() / 1000;
+
+        if (!entry || (entry.lastCheck + 3600 < currentTime)) {
+            const str = pgescape('SELECT ip FROM banned WHERE ip=%L', ip);
+            const res: QueryResult = await this.db.query(str);
+            this.banList[ip] = { lastCheck: currentTime, state: res.rowCount !== 0 };
+        }
+
+        return this.banList[ip].state ? Promise.reject() : Promise.resolve();
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,12 @@ import { WebHost } from '@microsoft/mixed-reality-extension-sdk';
 import { resolve as resolvePath } from 'path';
 import { dispatch, dispatchControl, dispatchStartup } from './dispatch';
 
-import Http from 'http';
+import Http, { IncomingMessage } from 'http';
 import HttpProxy from 'http-proxy';
 import QueryString from 'query-string';
 import WebSocket from 'ws';
+
+import DoorGuard from './DoorGuard';
 import PGBackend from './pg_backend';
 
 process.on('uncaughtException', err => console.log('uncaughtException', err));
@@ -22,11 +24,14 @@ const db = PGBackend.instance;
 // Initialize the submodules
 dispatchStartup();
 
+DoorGuard.init();
+
 // The port number we make available. Either use the PORT environment variable
 // (Heroku defines one) or take the standard 3901.
 const publicPort = Number(process.env.PORT || 3901);
 const mrePort = publicPort + 1;
 const controlPort = publicPort + 2;
+const banPort = publicPort + 3;
 
 // Start listening for connections, and serve static files
 const server = new WebHost({
@@ -44,6 +49,15 @@ controlserver.on('connection', (ws) => {
     ws.on('message', (payload) => dispatchControl(ws, payload as string));
 });
 
+// Yet another server that delays an incoming request and then denies it.
+const banServer = Http.createServer((req: Http.IncomingMessage, res: Http.ServerResponse) => {
+    setTimeout(() => {
+        res.writeHead(403, { 'Content-type': 'text/plain'});
+        res.write('Banned! Your IP address sent too many bogus requests that it had to be blacklisteed.');
+        res.end();
+    }, 50000);
+}).listen(banPort);
+
 // Use a lean HTTP proxy to multiplex the connections onto a single port, as follows:
 // http://.* --> localhost:3902 (the MRE WebHost)
 // ws://.*/control --> localhost:3903 (the control connection)
@@ -54,17 +68,20 @@ const proxy = HttpProxy.createProxyServer({
 const proxyServer = Http.createServer(
     (req, res) => {
         proxy.web(req, res, { target: `http://localhost:${mrePort}` });
-    }
-    );
+    });
 
 proxyServer.on('upgrade',
-    (req, socket, head) => {
+    (req: Http.IncomingMessage, socket, head) => {
         const query = QueryString.parseUrl(req.url);
-        if ((query.url as string) === '/control') {
-            proxy.ws(req, socket, head, { target: `ws://localhost:${controlPort}` });
-        } else {
-            proxy.ws(req, socket, head, { target: `ws://localhost:${mrePort}` });
-        }
+        DoorGuard.isAdmitted(req.connection.remoteAddress).then(() => {
+            if ((query.url as string) === '/control') {
+                proxy.ws(req, socket, head, { target: `ws://localhost:${controlPort}` });
+            } else {
+                proxy.ws(req, socket, head, { target: `ws://localhost:${mrePort}` });
+            }
+        }).catch(() => {
+            proxy.ws(req, socket, head, { target: `http://localhost:${banPort}` });
+        });
     });
 
 // Listen to the given port (as defined per environment like Heroku)
